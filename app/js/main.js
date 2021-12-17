d622d166e4d8bace00845ba50143b48d1be546a1
vsulimovvv <super.sulimov2014@yandex.ru>
2021-12-17 18:49:34 +0300

    added hover effects

--- app/js/main.js
+++ app/js/main.js
@@ -2,6 +2,7 @@ window.addEventListener('DOMContentLoaded', () => {
   var swiper = new Swiper(".swiper", {
     pagination: {
       el: ".swiper-pagination",
+      clickable: true,
     },
     navigation: {
       nextEl: ".swiper-button-next",
